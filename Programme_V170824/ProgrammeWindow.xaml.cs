f6d0a54c7e58901d0a4afca6da3621a26411d76e
HaleFeng <MrFeng@DESKTOP-BQQSTEB>
2017-09-25 07:57:03 +0800

    方案部分1

--- Programme_V170824/ProgrammeWindow.xaml.cs
+++ Programme_V170824/ProgrammeWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.IO;
+using System.Threading;
+
 
 namespace Programme_V170824
 {
@@ -21,15 +23,23 @@ namespace Programme_V170824
     /// </summary>
     public partial class ProgrammeWindow : Window
     {
+        private bool isok = true;//避免重复点击下载出现异常
+
+
         public ProgrammeWindow()
         {
             InitializeComponent();
+            string path = Directory.GetCurrentDirectory();
+            mediaElement.Source = new Uri(path + "//Image//loading.gif", UriKind.Absolute);
+            mediaElement.Stop();
+           
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            Download("D://" + floderName + "//" , fileName);
-
+            filePath = "D://" + floderName + "//";
+            if(isok)
+             Download();
         }
 
         string Username = "gzb1";
@@ -37,41 +47,103 @@ namespace Programme_V170824
         string ftpServerIP = "118.178.131.196:5001/冯帅帅";
         string floderName = "root";
         string fileName = "vcredist_x86.exe";
-        public void Download(string filePath,string fileName)
+        string filePath;
+        public void Download()
         {
-            FtpWebRequest reqFTP;
-            try
-            {
+            isok = false;
+            loadingStart();
+            FtpWebRequest reqFTP;    
+            reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + ftpServerIP + "/" + fileName));
+            reqFTP.Credentials = new NetworkCredential(Username, Password);
+            reqFTP.UseBinary = true;
+            reqFTP.Method = WebRequestMethods.Ftp.DownloadFile;
+            FtpWebResponse response = (FtpWebResponse)reqFTP.GetResponse();
+            long le = reqFTP.ContentLength;
+            ThreadPool.QueueUserWorkItem((obj) =>
+           {
+                
                 FileStream outputStream = new FileStream(filePath + "\\" + fileName, FileMode.Create);
-                reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + ftpServerIP + "/" + fileName));
-                reqFTP.Method = WebRequestMethods.Ftp.DownloadFile;
-                reqFTP.UseBinary = true;
-                reqFTP.Credentials = new NetworkCredential(Username, Password);
-                FtpWebResponse response = (FtpWebResponse)reqFTP.GetResponse();
                 Stream ftpStream = response.GetResponseStream();
                 long cl = response.ContentLength;
-                int bufferSize = 2048;
+                long progressBarValue = 0;
+                int bufferSize = 1024;
                 int readCount;
                 byte[] buffer = new byte[bufferSize];
                 readCount = ftpStream.Read(buffer, 0, bufferSize);
                 while (readCount > 0)
                 {
+
                     outputStream.Write(buffer, 0, readCount);
+                    progressBarValue += readCount;
+                //    pbDown.Dispatcher.BeginInvoke(new ProgressBarSetter(SetProgressBar), progressBarValue);
                     readCount = ftpStream.Read(buffer, 0, bufferSize);
 
-                }
-                MessageBox.Show("下载完成！");
+                }    
                 ftpStream.Close();
                 outputStream.Close();
                 response.Close();
+                isok = true;
+               inv();//跨线程调用mediaelement
+                },null);
 
-            }catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+
+           
+       //     MessageBox.Show("下载完成！");
+
+
+
+
+
+
+
+        }
+
+        /// <summary>
+        /// 声明委托 用于跨线程调用控件
+        /// </summary>
+        private delegate void mydelegate();
+        private void inv()
+        {
+            mediaElement.Dispatcher.Invoke(new mydelegate(loadingStop));
         }
 
 
-
+        /// <summary>
+        /// 下载结束  结束播放动画并隐藏控件
+        /// </summary>
+        private void loadingStop()
+        {
+            
+            mediaElement.Stop();
+            mediaElement.Visibility = Visibility.Hidden;
+        }
+        /// <summary>
+        /// 下载开始 开始播放gif
+        /// </summary>
+        private void loadingStart()
+        {
+            mediaElement.Visibility = Visibility.Visible;
+            mediaElement.Play();    
+        }
+       
+        /// <summary>
+        /// 播放结束后循环
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            //mediaElement.Stop();
+            mediaElement.Position = TimeSpan.MinValue;
+            mediaElement.Play();
+         
+        }
     }
+  
+
+ 
+
+
 }
+
+
--- Programme_V170824/Window1.xaml.cs
+++ Programme_V170824/Window1.xaml.cs
@@ -16,15 +16,98 @@ namespace Programme_V170824
 {
     /// <summary>
     /// Window1.xaml 的交互逻辑
+    /// 用做测试窗口
+    /// 方案窗口也做成跟产品一样的界面  侧面
     /// </summary>
     public partial class Window1 : Window
     {
+        string ftpServerIP = "118.178.131.196:5001/冯帅帅";
+        string ftpUserID = "gzb1";
+        string ftpPassword = "GZB2014.0";
         public Window1()
         {
             InitializeComponent();
 
 
-            
         }
+        /// <summary>
+        /// 上传
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_up_Click(object sender, RoutedEventArgs e)
+        {
+            FtpUpDown ftp = new FtpUpDown(ftpServerIP, ftpUserID, ftpPassword);
+            string localpath = @"D:\root\vcredist_x86.exe";//本地路径
+            string ftppath = ftpServerIP + @"\test";
+            bool bol = ftp.Upload(localpath, ftppath);
+            if (bol == true)
+                MessageBox.Show("上传成功");
+            else
+                MessageBox.Show("上传失败");
+
+        }
+        /// <summary>
+        /// 下载
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_down_Click(object sender, RoutedEventArgs e)
+        {
+            string errorinfo;
+            string localpath = @"D:\root\f1";
+            string filename = "vcredist_x86.exe";
+            FtpUpDown ftp = new FtpUpDown(ftpServerIP, ftpUserID, ftpPassword);
+            bool bol = ftp.Download(localpath, filename, out errorinfo);
+            if (bol == true)
+                MessageBox.Show("下载成功");
+            else
+                MessageBox.Show("下载失败：" + errorinfo + "");
+        }
+        /// <summary>
+        /// 获取文件列表
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_filelist_Click(object sender, RoutedEventArgs e)
+        {
+            FileDetail("test");
+        }
+
+
+        public void FileDetail(string floderName)
+        {
+            FtpUpDown ftpUpDown = new FtpUpDown(ftpServerIP, ftpUserID, ftpPassword);
+            Paragraph pa = new Paragraph();
+            Run r = new Run();
+            string[] str = ftpUpDown.GetFileList(floderName);
+            if (str != null)
+            {
+                foreach (string item in str)
+                {
+                    TreeViewItem treeviewitem1 = new TreeViewItem();
+                    treeviewitem1.Header = item;
+                  //？？  treeView.Items.Add(treeviewitem1);
+                    r.Text = item;
+                    textBox.AppendText(r.Text + "\n");
+                }
+            }
+            string[] filedetaillist = ftpUpDown.GetFilesDetailList(floderName);
+            if(filedetaillist == null)
+            {
+                return;
+            }
+            foreach (string mystr in filedetaillist)
+            {
+                if (mystr.Contains("<DIR>"))
+                {
+                    // FileDetail(mystr);
+                     MessageBox.Show(mystr);
+                }
+                    
+               // textBox.AppendText(mystr + "\n");
+            }
+        }
+
     }
 }
